8119c3307dd91fe36c1c9089da477b8a15205830
RakhmanIbrayev <rakhmanxibrayev@gmail.com>
2022-02-02 01:26:06 +0600

    finish the script

--- script.js
+++ script.js
@@ -60,4 +60,43 @@ function game () {
     }
 }
 
-//game();
+const scoreBoardTitle = document.querySelector(".score-board-title");
+const userWeapon = document.querySelector(".human .choice");
+const computerWeapon = document.querySelector(".computer .choice");
+const userScore = document.querySelector(".human span");
+const computerScore = document.querySelector(".computer span");
+let computerSelection;
+
+
+function findWeaponEmoji (selection) {
+    if (selection === "rock") {
+        return "✊";
+    } else if (selection === "paper") {
+        return "🤚"
+    }
+    return "✌";
+}
+
+function game (userSelection) {
+    computerSelection = computerPlay();
+    userWeapon.textContent = findWeaponEmoji(userSelection);
+    computerWeapon.textContent = findWeaponEmoji(computerSelection);
+    scoreBoardTitle.textContent = playOneRound(userSelection, computerSelection);
+    userScore.textContent = userPoints;
+    computerScore.textContent = computerPoints;
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    let index = 0;
+
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i] === button) {
+            index = i;
+        }
+    }
+
+    button.addEventListener("click", () => {
+        game(figures[index]);
+    });
+});
